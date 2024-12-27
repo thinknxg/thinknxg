edc5660d0f0994f166d95da15b5e1d530fec4a82
Rijin Raju <rijinraju856@gmail.com>
2024-12-28 01:32:58 +0530

    updated about section

--- frappe/public/js/frappe/ui/toolbar/about.js
+++ frappe/public/js/frappe/ui/toolbar/about.js
@@ -1,7 +1,7 @@
 frappe.provide("frappe.ui.misc");
 frappe.ui.misc.about = function () {
 	if (!frappe.ui.misc.about_dialog) {
-		var d = new frappe.ui.Dialog({ title: __("Frappe Framework") });
+		var d = new frappe.ui.Dialog({ title: __("thinknxg") });
 
 		$(d.body).html(
 			repl(
@@ -9,18 +9,11 @@ frappe.ui.misc.about = function () {
 					<p>${__("Open Source Applications for the Web")}</p>
 					<p><i class='fa fa-globe fa-fw'></i>
 						${__("Website")}:
-						<a href='https://frappeframework.com' target='_blank'>https://frappeframework.com</a></p>
+						<a href='https://thinknxg.com' target='_blank'>https://thinknxg.com</a></p>
 					<p><i class='fa fa-github fa-fw'></i>
 						${__("Source")}:
-						<a href='https://github.com/frappe' target='_blank'>https://github.com/frappe</a></p>
-					<p><i class='fa fa-graduation-cap fa-fw'></i>
-						Frappe School: <a href='https://frappe.school' target='_blank'>https://frappe.school</a></p>
-					<p><i class='fa fa-linkedin fa-fw'></i>
-						Linkedin: <a href='https://linkedin.com/company/frappe-tech' target='_blank'>https://linkedin.com/company/frappe-tech</a></p>
-					<p><i class='fa fa-twitter fa-fw'></i>
-						Twitter: <a href='https://twitter.com/frappetech' target='_blank'>https://twitter.com/frappetech</a></p>
-					<p><i class='fa fa-youtube fa-fw'></i>
-						YouTube: <a href='https://www.youtube.com/@frappetech' target='_blank'>https://www.youtube.com/@frappetech</a></p>
+						<a href='https://github.com/thinknxg' target='_blank'>https://github.com/thinknxg</a></p>
+					
 					<hr>
 					<h4>${__("Installed Apps")}</h4>
 					<div id='about-app-versions'>${__("Loading versions...")}</div>
@@ -32,7 +25,7 @@ frappe.ui.misc.about = function () {
 						</b>
 					</p>
 					<hr>
-					<p class='text-muted'>${__("&copy; Frappe Technologies Pvt. Ltd. and contributors")} </p>
+					<p class='text-muted'>${__("&copy; thinknxg Technologies Pvt. Ltd. and contributors")} </p>
 					</div>`,
 				frappe.app
 			)
