e30e4c80b3b179475bc70aab6dae2b0114e75d38
Rijin Raju <rijinraju856@gmail.com>
2025-01-31 23:07:24 +0530

    Fix: updated icons

--- frappe/website/doctype/website_settings/website_settings.py
+++ frappe/website/doctype/website_settings/website_settings.py
@@ -249,7 +249,7 @@ def get_website_settings(context=None):
 		context.theme = get_active_theme() or frappe._dict()
 
 	if not context.get("favicon"):
-		context["favicon"] = "/assets/frappe/images/frappe-favicon.svg"
+		context["favicon"] = "/assets/frappe/images/thinknxg-favicon.png"
 
 	if settings.favicon and settings.favicon != "attach_files:":
 		context["favicon"] = settings.favicon
