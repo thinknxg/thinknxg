2e3008c6e2ddb4c13caeb6d90b730c735d5943e1
Rijin Raju <rijinraju856@gmail.com>
2025-11-23 00:33:10 +0530

    fix: updated title and logo

--- frappe/hooks.py
+++ frappe/hooks.py
@@ -3,11 +3,11 @@ import os
 from . import __version__ as app_version
 
 app_name = "frappe"
-app_title = "Frappe Framework"
-app_publisher = "Frappe Technologies"
+app_title = "thinkNXG Framework"
+app_publisher = "thinkNXG Technologies"
 app_description = "Full stack web framework with Python, Javascript, MariaDB, Redis, Node"
 app_license = "MIT"
-app_logo_url = "/assets/frappe/images/frappe-framework-logo.svg"
+app_logo_url = "/assets/frappe/images/thinkNXG-logo.jpg"
 develop_version = "15.x.x-develop"
 app_home = "/app/build"
 
