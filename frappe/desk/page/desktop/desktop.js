5badcc5b24a729195c09b1807963ab89d66273cc
Rijin Raju <rijinraju856@gmail.com>
2026-06-06 23:46:11 +0530

    fix: updated sidebar & desk support

--- frappe/desk/page/desktop/desktop.js
+++ frappe/desk/page/desktop/desktop.js
@@ -457,9 +457,9 @@ class DesktopPage {
 			},
 			{
 				icon: "support",
-				label: "Frappe Support",
+				label: "thinkNXG Support",
 				onClick: function () {
-					window.open("https://support.frappe.io/help", "_blank");
+					window.open("https://support.thinknxg.net", "_blank");
 				},
 			},
 			{
